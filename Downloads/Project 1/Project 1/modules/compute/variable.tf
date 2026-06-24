026bc43ef7e0c79c98b2ca0fb027a528a579876d
anhpn <anhpn@phx-smartschool.com>
2026-06-24 11:18:37 +0700

    Fix long folder va update code

---
+++ Downloads/Project 1/Project 1/modules/compute/variable.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  type = string
+  default = "ap-southeast-1"
+}
+
+variable "image_id" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+}
+variable "key_name" {
+  type = string
+  description = "name of the keypair to use for the instance"
+  nullable = false
+}
+variable "instance_type" {
+  type        = string
+  description = "Type of EC2 instance to launch. Example: t2.micro"
+  default = "t3.micro"
+}
+
+variable "subnet_id" {
+  type = string
+  description = "The subnet ID to launch in"
+  nullable = false
+}
+
+variable "ec2_security_group_ids" {
+  type = list(string)
+  nullable = false
+}
+variable "iam_instance_profile_name" {
+  type = string
+  description = "The name of the IAM instance profile to associate with the instance"
+  nullable = false
+}
+
+variable "ecr_image_url" {
+  type        = string
+  description = "Full ECR image URI (registry/repo:tag) for docker pull/run"
+}
+
---
+++ Downloads/Project 1/modules/compute/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "demo-instance" {
+  ami                    = var.image_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.ec2_security_group_ids
+  subnet_id              = var.subnet_id
+  iam_instance_profile   = var.iam_instance_profile_name
+  user_data_replace_on_change = true
+  tags = {
+    Name = "HK-ECO-demo-instance"
+  }
+}  
+resource "aws_eip" "HK-ECO-eip" {
+  instance = aws_instance.demo-instance.id
+}
+
